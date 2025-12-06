eef09a96b5ac02e18bdec0a7140048087a9319bf
Pritish Ramesh Pawar <72183570+pritish9156@users.noreply.github.com>
2025-12-07 00:36:25 +0530

    First Commit creating FirstBit Workspace
    FirstBit Workspace contains C Programming Assignments in this commit which includes 10 questions with their solution and output with one hand traced solution pdf.

---
+++ FirstBit Workspace/C Programming/Assignments/Assignment 0/Q1/Q1.c
@@ -0,0 +1,19 @@
+//Write a C program to add two integers and display the result
+
+#include<stdio.h>
+
+void main(){
+	
+	//initiallization
+	int integer1 = 20;
+	int integer2 = 30;
+	
+	//declaration
+	int Addition_result;
+	
+	//performing operation to find a operational value
+	Addition_result = integer1 + integer2;
+	
+	//printing result on output screen
+	printf("Addition of %d and %d is %d",integer1, integer2, Addition_result);
+}
---
+++ FirstBit Workspace/C Programming/Assignments/Assignment 0/Q6/Q6.c
@@ -0,0 +1,21 @@
+//Write a C program to find the square and cube of a given number.
+
+#include<stdio.h>
+
+void main(){
+	
+	//initiallization
+	int num = 2;
+	
+	//declaration
+	int square, cube;
+	
+	//performing operation
+	square = num * num;
+	cube = num * num * num;
+	
+	//printing output
+	printf("square of %d is %d",num, square);
+	printf("\ncube of %d is %d",num, cube);
+	
+}
