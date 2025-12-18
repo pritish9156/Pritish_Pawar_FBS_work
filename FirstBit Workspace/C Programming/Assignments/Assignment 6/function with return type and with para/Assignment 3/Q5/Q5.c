5b920e58e147fdd7a3a762fffe926b9912ddc40c
Pritish Ramesh Pawar <72183570+pritish9156@users.noreply.github.com>
2025-12-18 20:44:41 +0530

    Assignment 6
    contains programs for function with return type and with parameter.

---
+++ FirstBit Workspace/C Programming/Assignments/Assignment 6/function with return type and with para/Assignment 3/Q5/Q5.c
@@ -0,0 +1,47 @@
+//Check the given number is Armstrong number or not.. Input: n = 153 Output: Armstrong
+
+#include<stdio.h>
+
+int isArmstrong(int);
+void main(){
+	int n = 153;
+	if(isArmstrong(n))
+	   printf("Armstrong");
+	else
+	   printf("Not Armstrong");
+	   
+}
+
+int isArmstrong(int n){
+	
+	int sum=0, orig = n, digit=0;
+	
+	while(n > 0){
+		n = n/10;
+		digit++;
+	}
+	
+	n = orig;
+	
+	while(n > 0){
+		int r1 = n%10;
+		
+		int power = 1;
+		int i=1;
+		while(i<=digit){
+			power = power * r1;
+			i++;
+		}
+		
+		sum = sum + power;
+		n = n/10;
+	}
+	
+	if(orig == sum){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+	
+}
