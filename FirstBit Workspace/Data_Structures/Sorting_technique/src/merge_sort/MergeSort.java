3473dee89db2f52ea2192b74be579bc637d6c5dc
Pritish Ramesh Pawar <pritishpawar15@gmail.com>
2026-04-09 01:11:56 +0530

    implemented quick sort algorithm under DSA sorting technique

---
+++ FirstBit Workspace/Data_Structures/Sorting_technique/src/merge_sort/MergeSort.java
@@ -0,0 +1,102 @@
+package merge_sort;
+
+import java.util.Scanner;
+
+public class MergeSort{
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("How many Numbers you want to store: ");
+		int arraySize = sc.nextInt();
+		
+		int[] array = new int[arraySize];
+		
+		System.out.print("\nEnter " + arraySize + " numbers to store: ");
+		
+		for(int i=0; i<array.length; i++) {
+			array[i] = sc.nextInt();
+		}
+		
+		System.out.print("\nNumbers stored: [");
+		
+		for(int i=0; i<array.length; i++) {
+			if(i==array.length-1)
+				System.out.println(array[i] + "]");
+			else
+				System.out.print(array[i] + ", ");
+		}
+		
+		System.out.println("\nImplementing Merge sort to sort the array...");
+		
+		mergeSort(array, 0, array.length-1);
+		
+		System.out.print("\nAfter Sorting: [");
+		
+		for(int i=0; i<array.length; i++) {
+			if(i==array.length-1)
+				System.out.println(array[i] + "]");
+			else
+				System.out.print(array[i] + ", ");
+		}
+		
+	}//main method ends here
+	
+	static void combine(int[] array, int beg, int mid, int end) {
+		
+		int l1 = mid-beg+1;
+		int l2 = end-mid;
+		
+		int[] left = new int[l1];
+		int[] right = new int[l2];
+		
+		for(int i=0; i<left.length; i++) {
+			left[i] = array[beg + i];
+		}
+		
+		for(int i=0; i<right.length; i++) {
+			right[i] = array[mid+1+i];
+		}
+		
+		int k=0, j=0, i=beg;
+		
+			while(k<l1 && j<l2) {
+				if(left[k]<right[j]) {
+					array[i] = left[k];
+					k++;
+				}else {
+					array[i] = right[j];
+					j++;
+				}
+				i++;
+			}
+			
+			while(k < l1) {
+				array[i] = left[k];
+				k++;
+				i++;
+			}
+			
+			while(j < l2) {
+				array[i] = right[j];
+				j++;
+				i++;
+			}
+		}
+	
+	static void mergeSort(int[] array, int beg, int end) {
+		
+		if(beg<end) {
+			
+			int mid = (beg + end)/2;
+			
+			mergeSort(array, beg, mid);
+			mergeSort(array, mid+1, end);
+			combine(array, beg, mid, end);
+			
+		}
+		
+	}
+	
+} //mergeSort class ends here
---
+++ FirstBit Workspace/Data_Structures/Sorting_technique/src/quick_sort/QuickSort.java
@@ -0,0 +1,86 @@
+package quick_sort;
+
+import java.util.Scanner;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("How many Numbers you want to store: ");
+		int arraySize = sc.nextInt();
+		
+		int[] array = new int[arraySize];
+		
+		System.out.print("\nEnter " + arraySize + " numbers to store: ");
+		
+		for(int i=0; i<array.length; i++) {
+			array[i] = sc.nextInt();
+		}
+		
+		System.out.print("\nNumbers stored: [");
+		
+		for(int i=0; i<array.length; i++) {
+			if(i==array.length-1)
+				System.out.println(array[i] + "]");
+			else
+				System.out.print(array[i] + ", ");
+		}
+		
+		System.out.println("\nImplementing Quick sort to sort the array...");
+		
+		quickSort(array, 0, array.length-1);
+		
+		System.out.print("\nAfter Sorting: [");
+		
+		for(int i=0; i<array.length; i++) {
+			if(i==array.length-1)
+				System.out.println(array[i] + "]");
+			else
+				System.out.print(array[i] + ", ");
+		}
+
+	} //main ends here
+	
+	static void quickSort(int array[], int beg, int end) {
+		
+		if(beg<end) {
+		
+			int d = divide(array, beg, end);
+			quickSort(array, beg, d-1);
+			quickSort(array, d+1, end);
+		
+		}	
+	}
+	
+	static int divide(int[] array, int beg, int end) {
+		
+		int pivot = array[beg];
+		int i = beg+1, j = end;
+		
+		while(i<=j) {
+			
+			while(i<=end && array[i] <= pivot) {
+				i++;
+			}
+			
+			while(j>=beg && array[j] > pivot) {
+				j--;
+			}
+			
+			if(i<j) {
+				int temp = array[j];
+				array[j] = array[i];
+				array[i] = temp;
+			}
+
+		}
+		
+		int temp = array[beg];
+		array[beg] = array[j];
+		array[j] = temp;
+		return j;
+	}
+
+}//class ends here
