2316868a9f0563ae583516a6a1e006b5cd12a471
Pritish Ramesh Pawar <pritishpawar15@gmail.com>
2026-04-14 18:36:18 +0530

    Queue Data Structure implementation - simple queue

---
+++ FirstBit Workspace/Data_Structures/Data Structures/src/queue/Queue.java
@@ -0,0 +1,68 @@
+package queue;
+
+public class Queue {
+	
+	int[] arr;
+	int front, rear;
+	
+	public Queue(int size) {
+		arr = new int[size];
+		front=-1;
+		rear=-1;
+	}
+	
+	public boolean isEmpty() {
+		if(rear==-1) return true;
+		else return false;
+	}
+	
+	public boolean isFull() {
+		if(rear==arr.length-1) return true;
+		else return false;
+	}
+	
+	public void enqueue(int ele) {
+		if(isFull()) {
+			System.out.println("Queue is Full");
+			return;
+		}
+		
+		if(isEmpty()) {
+			front++;
+		}
+
+		arr[++rear]=ele;
+	}
+	
+	public void dequeue() {
+		if(isEmpty()) {
+			System.out.println("Queue is Empty");
+			return;
+		}
+		
+		if(front==rear) {
+			front=-1;
+			rear=-1;
+		}
+		else
+			front++;
+			
+	}
+	
+	public void display() {
+		
+		if(isEmpty()) {
+			System.out.println("Queue is Empty");
+			return;
+		}
+		
+		System.out.print("[");
+		for(int i=front; i<=rear; i++) {
+			if(i==rear)
+				System.out.print(arr[i]+"]\n");
+			else
+			System.out.print(arr[i]+", ");
+		}
+	}
+	
+}
---
+++ FirstBit Workspace/Data_Structures/Data Structures/src/queue/QueueImplementation.java
@@ -0,0 +1,51 @@
+package queue;
+
+import java.util.Scanner;
+
+public class QueueImplementation {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("How many elements you want to store: ");
+		int arrSize = sc.nextInt();
+		
+		Queue queue = new Queue(arrSize);
+		int choice;
+		
+		do {
+			System.out.println("\n1.Enqueue\n2.Dequeue\n3.Display\n4.Exit");
+			System.out.println("Enter Your Choice: ");
+			choice = sc.nextInt();
+			
+			switch(choice) {
+				case 1:{
+					
+					System.out.println("\nEnter a number to insert: ");
+					int numToEnqueue = sc.nextInt();
+					queue.enqueue(numToEnqueue);
+					break;
+					
+				}
+				case 2:{
+					
+					queue.dequeue();
+					break;
+					
+				}
+				case 3:{
+					
+					queue.display();
+					break;
+					
+				}
+				case 4:{
+					System.out.println("Exited...!");
+					break;
+				}
+			}
+		}while(choice!=4);
+	}
+
+}
